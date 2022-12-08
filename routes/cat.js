44bfc6266d026961b34fed03f8ba898b80d4bc13
skrhndrn <handarenisekar@gmail.com>
2022-12-08 21:18:32 +0700

    cat data features done

--- routes/cat.js
+++ routes/cat.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const CatController = require('../controllers/cat');
 
-router.get("/", CatController.showAllCats);
+router.get("/", CatController.getAllCats);
 
 module.exports = router;
