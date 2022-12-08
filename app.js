029176c8bfad33bdae96a49184cf8dfab3fb990a
skrhndrn <handarenisekar@gmail.com>
2022-12-08 13:12:11 +0700

    update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port =  process.env.port || 3000;
 
 app.use(cors());
 app.use(express.json());
